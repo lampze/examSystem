636341587e0b4a7e3d9292fe71afd78af659a789
shirui <1229408499@qq.com>
2018-01-14 12:12:51 +0800

    给按钮加了监听事件，还有随机显示题目。

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 function getElements(dom) {
-  return document.getElementsByClassName(dom)[0];
+  return document.getElementsByClassName(dom);
 }
 
-var subject = getElements("subject");
-var optA = getElements("opt-A");
-var optB = getElements("opt-B");
-var optC = getElements("opt-C");
-var optD = getElements("opt-D");
+var subject = getElements("subject")[0];
+var option = getElements("option");
+
+var user = {};
+
+for(let i = 0; i < option.length; i++) {
+  var obj = option[i];
+  obj.onclick = function() {
+    var opt = option[i];
+    opt.classList.remove("btn-default");
+    opt.classList.add("btn-primary");
+
+    for(let j = 0; j < option.length; j++) {
+      if(j != i) {
+        var opts = option[j];
+        opts.classList.remove("btn-primary");
+        opts.classList.add("btn-default");
+      }
+    }
+  };
+}
+
+option.onclick = function() {
+  optA.classList.remove("btn-default");
+  optA.classList.add("btn-primary");
+};
 
 function displaySubject(subDom, dataObj) {
   let str = dataObj.head + "\n\n";
@@ -15,4 +36,47 @@ function displaySubject(subDom, dataObj) {
   }
   subDom.innerText = str;
 }
-displaySubject(subject, examData[1000]);
+
+var cloneObj = function (obj) {
+    var newObj = {};
+    if (obj instanceof Array) {
+        newObj = [];
+    }
+    for (var key in obj) {
+        var val = obj[key];
+        newObj[key] = typeof val === 'object' ? cloneObj(val) : val;
+    }
+    return newObj;
+};
+
+function random(min, max) {
+  return parseInt(Math.random()*(max-min+1)+min,10);
+}
+
+function getRandSubject(data, time) {
+  var randData = [];
+  var allRandNum = [];
+  if(time > data.length) {
+    return false;
+  }
+  for(let i = 0; i < time; i++) {
+    isRepeat = true;
+    while(isRepeat) {
+      randNum = random(0,data.length);
+      isRepeat = false;
+      for(let j = 0; j < allRandNum.length; j++) {
+        if(randNum == allRandNum[j]) {
+          isRepeat = true;
+        }
+      }
+    }
+    allRandNum.push(randNum);
+    var obj = cloneObj(data[randNum]);
+    obj.number = randNum;
+    randData.push(obj);
+  }
+  return randData;
+}
+
+var randSubject = getRandSubject(examData, 50);
+displaySubject(subject, randSubject[0]);
