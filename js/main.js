01406f1ccb8a477f6c4589aed8166074b54df0fc
shirui <1229408499@qq.com>
2018-01-22 18:01:42 +0800

    修了一些bug，减少了examdata的大小。

--- js/main.js
+++ js/main.js
@@ -95,7 +95,53 @@ var user = {};
 
 
 
-subjectTotal.innerText = examData.length + 1;
+// 为ie10以下做classList做兼容 -开始-
+
+if (!("classList" in document.documentElement)) {
+  Object.defineProperty(HTMLElement.prototype, 'classList', {
+    get: function() {
+      var self = this;
+      function update(fn) {
+        return function(value) {
+          var classes = self.className.split(/\s+/g),
+              index = classes.indexOf(value);
+
+          fn(classes, index, value);
+          self.className = classes.join(" ");
+        };
+      }
+
+      return {
+        add: update(function(classes, index, value) {
+          if (!~index) classes.push(value);
+        }),
+
+        remove: update(function(classes, index) {
+          if (~index) classes.splice(index, 1);
+        }),
+
+        toggle: update(function(classes, index, value) {
+          if (~index)
+            classes.splice(index, 1);
+          else
+            classes.push(value);
+        }),
+
+        contains: function(value) {
+          return !!~self.className.split(/\s+/g).indexOf(value);
+        },
+
+        item: function(i) {
+          return self.className.split(/\s+/g)[i] || null;
+        }
+      };
+    }
+  });
+}
+
+// 为ie10以下做classList做兼容-结束-
+
+subjectTotal.innerText = examData.length;
 
 for(let i = 0; i < option.length; i++) {
   option[i].onclick = function() {
